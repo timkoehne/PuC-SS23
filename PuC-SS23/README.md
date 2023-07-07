ab1cdef16952e425abb1cfe1c0759127898f500a
Tim Köhne <tim.koehne97@gmail.com>
2023-07-07 17:49:03 +0200

    added socket server that receives text to parse

--- PuC-SS23/README.md
+++ PuC-SS23/README.md
@@ -1,4 +1,7 @@
-Programmiersprachen und Compilerbau - SS2023
+Semantic Highlighting for PuC-Lang
 ===
 
-Slides befinden sich im `notes/` Verzeichnis.
+# TODO:
+- send formatting data as json in main.kt
+- convert formatting to semantic token builder entries in extension.ts
+- add remaining formatting rules in listener.kt
--- PuC-SS23/compiler/src/main/kotlin/main.kt
+++ PuC-SS23/compiler/src/main/kotlin/main.kt
@@ -1,8 +1,35 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+import java.net.Socket
+
+
 fun main() {
-    val parsed = parseFile("types.puc")
-    val (type, errors) = Typechecker().inferProg(parsed)
-    errors.forEach { println(it) }
-    val evaled = closureEval(parsed)
-    print("$evaled")
-    println(": ${type.print()}")
+    val server = ServerSocket(3000)
+
+    while(true){
+        val client = server.accept()
+        Thread{ handleClient(client)}.start()
+    }
+}
+
+fun handleClient(client: Socket){
+    println("Connection established")
+    val output = PrintWriter(client.getOutputStream(), true)
+    val input = BufferedReader(InputStreamReader(client.inputStream))
+    val textReceived = input.readText()
+    println(textReceived)
+
+    val highlightListener = HighlightListener()
+    highlightListener.parseAndWalk(textReceived)
+
+    for (highlight in highlightListener.highlights)
+        println(highlight)
+
+    //TODO highlighter.highlights to json before sending
+
+    val test = highlightListener.highlights
+    println("Responding with $test")
+    output.println(test)
 }
