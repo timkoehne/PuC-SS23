4c0331d355fe4b225cabe7c745c2a362709ba371
Tim Köhne <tim.koehne97@gmail.com>
2023-07-16 17:16:22 +0200

    improve the way line positions are calculated

--- PuC-SS23/compiler/src/main/kotlin/main.kt
+++ PuC-SS23/compiler/src/main/kotlin/main.kt
@@ -3,6 +3,7 @@ import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.ServerSocket
 import java.net.Socket
+import javax.sound.sampled.Line
 
 fun main() {
     val server = ServerSocket(3000)
@@ -13,62 +14,71 @@ fun main() {
     }
 }
 
-fun convertStringIndicesToLineIndices(highlightListener: HighlightListener, textReceived: String): MutableList<Highlight> {
-    val newHighlights = mutableListOf<Highlight>()
-    val lines = textReceived.split("\n")
-    for (highlight in highlightListener.highlights) {
-        var lineStart = 0
-        var lineEnd: Int
-        var lineNum = 0
-        var startFound = false
-        for (lineText in lines) {
-            lineEnd = lineStart + lineText.length + lineNum //not quite sure why we need + lineNum, but it works
-            if (highlight.start in lineStart..lineEnd) {
-                startFound = true
-            }
 
-            if (startFound) {
-                if (highlight.end <= lineEnd) {
-                    highlight.lineNum = lineNum
-//                    println("zeile: $lineNum")
+data class LinePosition(val line: Int, val pos: Int) {
+    override fun toString(): String {
+        return "line: $line, pos: $pos"
+    }
+}
 
-                    if (highlight.start in lineStart..lineEnd && highlight.end in lineStart..lineEnd) {
-                        newHighlights.add(Highlight(
-                                highlight.type,
-                                highlight.start - (lineStart + lineNum), //not quite sure why we need + lineNum, but it works
-                                0,
-                                lineNum,
-                                highlight.end - highlight.start + 1,
-                        ))
-                    } else {
-                        newHighlights.add(Highlight(
-                                highlight.type,
-                                0,
-                                0,
-                                lineNum,
-                                lineText.length - 1,
-                        ))
-                    }
-                    break
-                } else {
-                    newHighlights.add(Highlight(
-                            highlight.type,
-                            0,
-                            0,
-                            lineNum,
-                            lineText.length - 1,
-                    ))
-                    highlight.lineNum = lineNum
-//                    println("zeile: $lineNum")
-                    highlight.start = 0
-//                    println("start: 0")
-                    highlight.length = lineText.length - 1
-//                    println("length: ${lineText.length - 1}")
-                }
-            }
-            lineStart += lineText.length
-            lineNum++
+fun findStartAndEndPos(highlight: Highlight, lines: List<String>, lineEnding: String): Pair<LinePosition, LinePosition> {
+    var startPos: LinePosition? = null
+    var endPos: LinePosition? = null
+
+    var lineStart = 0
+    var lineEnd: Int
+    for ((lineNum, lineText) in lines.withIndex()) {
+        lineEnd = lineStart + lineText.length
+
+        if (highlight.start in lineStart..lineEnd) {
+            startPos = LinePosition(lineNum, highlight.start - lineStart)
         }
+
+        if (highlight.end in lineStart..lineEnd) {
+            endPos = LinePosition(lineNum, highlight.end - lineStart)
+        }
+
+        lineStart += lineText.length + lineEnding.length
+    }
+
+    if ((startPos == null) || (endPos == null))
+        throw Error("Start or End Position is null")
+    return Pair(startPos, endPos)
+}
+
+fun convertStringIndicesToLineIndices(highlights: List<Highlight>, textReceived: String): MutableList<Highlight> {
+    val newHighlights = mutableListOf<Highlight>()
+
+    val lineEnding = if (textReceived.contains("\r\n")) "\r\n" else "\n"
+
+    if (lineEnding == "\r\n") {
+        println("lineEnding \\r\\n")
+    } else {
+        println("lineEnding \\n")
+    }
+
+
+    val lines = textReceived.split(lineEnding)
+
+    for (highlight in highlights) {
+
+        var (startPos, endPos) = findStartAndEndPos(highlight, lines, lineEnding)
+
+        for (lineNum in startPos.line..endPos.line) {
+            var start = 0
+            var end = lines[lineNum].length
+
+            if (startPos.line == lineNum) {
+                start = startPos.pos
+            }
+            if (endPos.line == lineNum) {
+                end = endPos.pos
+            }
+
+//            println("type ${highlight.type}: start: $start, end: $end")
+            newHighlights.add(Highlight(highlight.type, start, end, lineNum, end - start))
+        }
+
     }
     return newHighlights
 }
@@ -82,10 +92,18 @@ fun handleClient(client: Socket) {
     val highlightListener = HighlightListener()
     highlightListener.parseAndWalk(textReceived)
 
-    val convertedHighlights = convertStringIndicesToLineIndices(highlightListener, textReceived)
+    val convertedHighlights = convertStringIndicesToLineIndices(highlightListener.highlights, textReceived)
     for (highlight in convertedHighlights) {
         println(highlight)
     }
     println("Responding with $convertedHighlights")
     output.println(convertedHighlights)
+
+
+//    val highlights = convertStringIndicesToLineIndices2(listOf(Highlight("keyword", 110, 121, 0, 0)), textReceived)
+//    for (highlight in highlights)
+//        print(highlight)
+//    output.println(highlights)
+
+
 }
